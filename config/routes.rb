348930cd6e9ea9a3af0f611f7f56cfe804ef3fbd
mbialowas10 <mbialowas@rrc.ca>
2023-01-18 16:14:30 -0600

    Added Movies and ProductionCompanies Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'production_companies/index'
+  get 'production_companies/show'
+  get 'movies/index'
+  get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get /segment, to:controller#action, as: namedReference # e.g companies_path
+  #   GET "/movies"
+  #   GET /movies/:index
+  #   GET /production_companies
+  #   GET clear/production_companies/:id
 end
