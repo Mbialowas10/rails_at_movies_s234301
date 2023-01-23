d845b8eaceeadbfb03a56363f6ea1916a6d60827
mbialowas10 <mbialowas@rrc.ca>
2023-01-23 12:23:32 -0600

    m2m implemented b/w Movies and Genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ require 'csv'
 Movie.delete_all
 ProductionCompany.delete_all
 Page.delete_all
+MovieGenre.delete_all
+Genre.delete_all
 
 filename = Rails.root.join('db/top_movies.csv')
 puts "Loading Movie the csv file: #{filename}."
@@ -22,6 +24,14 @@ movies.each do |m|
       average_vote: m['avg_vote']
     )
     puts "Invalid movie #{m['original_title']}" unless movie&.valid?
+
+    genres = m["genre"].split(",").map(&:strip)
+
+    genres.each do |genre_name|
+      genre = Genre.find_or_create_by(name: genre_name)
+
+      MovieGenre.create(movie:movie, genre: genre)
+    end
   else
     puts "invalid production company #{m['production_company']} for movie #{m['original_title']}."
   end
@@ -40,3 +50,5 @@ Page.create(
 
 puts "Created #{ProductionCompany.count} Production Companies"
 puts "Created #{Movie.count} movies."
+puts "Created #{Genre.count} Genres."
+puts "Created #{MovieGenre.count} Movie Genres."
